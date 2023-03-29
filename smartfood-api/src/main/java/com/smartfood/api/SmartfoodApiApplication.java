99b0387437de15b1cc30dba75e2ae0be128b60ed
doomspook <amorim32@hotmail.com>
2023-03-29 11:46:14 -0300

    create repository

---
+++ smartfood-api/src/main/java/com/smartfood/api/SmartfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.smartfood.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartfoodApiApplication.class, args);
+	}
+
+}
---
+++ smartfood-api/src/test/java/com/smartfood/api/SmartfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartfood.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
